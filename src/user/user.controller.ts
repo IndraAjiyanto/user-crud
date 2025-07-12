4758830d51671da5b147e4fff8fa9b686f2f8bbc
IndraAjiyanto <indraajiyanto052@gmail.com>
2025-07-13 04:39:56 +0700

    get user by id

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from "@prisma/client";
@@ -6,6 +6,18 @@ import { User } from "@prisma/client";
 @Controller('users')
 export class UserController{
     constructor(private readonly userService: UserService) {}
+
+    @Get()
+    async getAllUser(): Promise<User[]>{
+        const users = await this.userService.getAllUsers();
+        return users;
+    }
+
+    @Get(':id')
+    async getUserById(@Param('id') id :number): Promise<User | null>{
+        const user = await this.userService.getUserById(Number(id));
+        return user;
+    }
 
     @Post()
     async createUser(@Body() userData: CreateUserDto): Promise<User>{
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "src/common/prisma/prisma.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from "@prisma/client";
@@ -6,6 +6,21 @@ import { User } from "@prisma/client";
 @Injectable()
 export class UserService {
     constructor( protected prismaservice: PrismaService) {}
+
+    async getAllUsers(): Promise<User[]>{
+        const users = await this.prismaservice.user.findMany();
+        return users;
+    }
+
+    async getUserById(id: number): Promise<User | null>{
+        const user = await this.prismaservice.user.findUnique({
+            where : { id },
+        });
+        if(!user){
+            throw new NotFoundException('user not found');
+        }
+        return user;
+    }
 
     async createUser(UserData: CreateUserDto): Promise<User>{
         const user = await this.prismaservice.user.create({
